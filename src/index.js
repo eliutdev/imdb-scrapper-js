4ac377ba9affda2a361d37a7a039069a8efcd438
REG <91336300+simplestupide@users.noreply.github.com>
2021-09-25 00:00:07 +0200

    chore: first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import request from "request-promise";
+import cheerio from "cheerio";
+
+const imdbScrapper = async (id) => {
+    const $ = await request({
+        uri: `https://www.imdb.com/title/${id}`,
+        transform: (body) => cheerio.load(body),
+    });
+
+    const data = {
+        title: $("title").text(),
+        description: $('meta[name="description"]').attr("content"),
+        image: $('meta[property="og:image"]').attr("content"),
+        type: $('meta[property="og:type"]').attr("content"),
+        userRating: $(
+            '[data-testid="hero-rating-bar__aggregate-rating"] [data-testid="hero-rating-bar__aggregate-rating__score"] span:nth-child(1)'
+        ).text(),
+        genres: $('[data-testid="genres"] span')
+            .map((_, el) => $(el).text())
+            .get(),
+        director: $(
+            '[data-testid="title-pc-principal-credit"] .ipc-metadata-list-item__list-content-item'
+        ).text(),
+    };
+
+    if (data.type === "video.movie") {
+        data.releaseDate = $(
+            '[data-testid="hero-title-block__metadata"] .ipc-inline-list__item:nth-child(1) a'
+        ).text();
+        data.pgRating = $(
+            '[data-testid="hero-title-block__metadata"] .ipc-inline-list__item:nth-child(2) a'
+        ).text();
+        data.duration = $(
+            '[data-testid="hero-title-block__metadata"] .ipc-inline-list__item:nth-child(3)'
+        ).text();
+    }
+
+    return data;
+};
+
+export default imdbScrapper;
